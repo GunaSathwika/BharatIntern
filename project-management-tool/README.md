c6285555bb7a90edd6a490e78e4c46de53e01fa0
GunaSathwika <141224178+GunaSathwika@users.noreply.github.com>
2023-10-27 01:43:55 +0530

    Add files via upload

---
+++ project-management-tool/README.md
@@ -0,0 +1,4 @@
+# Project Management Tool 
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Guna-Sathwika-Gone/pen/abXvyNK](https://codepen.io/Guna-Sathwika-Gone/pen/abXvyNK).
+
---
+++ project-management-tool/dist/script.js
@@ -0,0 +1,54 @@
+// Sample data for projects and tasks
+const projects = [
+    { id: 1, name: "Project 1" },
+    { id: 2, name: "Project 2" },
+    // Add more projects as needed
+];
+
+const tasks = [
+    { id: 1, project: 1, title: "Task 1", assignedTo: "User 1" },
+    { id: 2, project: 1, title: "Task 2", assignedTo: "User 2" },
+    // Add more tasks as needed
+];
+
+// Function to display the list of projects
+function displayProjects() {
+    const projectList = document.getElementById("projectList");
+    projectList.innerHTML = "";
+    projects.forEach(project => {
+        const projectItem = document.createElement("div");
+        projectItem.innerHTML = `<p>${project.name}</p>`;
+        projectList.appendChild(projectItem);
+    });
+}
+
+// Function to display the list of tasks
+function displayTasks() {
+    const taskList = document.getElementById("taskList");
+    taskList.innerHTML = "";
+    tasks.forEach(task => {
+        const taskItem = document.createElement("div");
+        taskItem.innerHTML = `<p>${task.title} (Project ${task.project}, Assigned to: ${task.assignedTo})</p>`;
+        taskList.appendChild(taskItem);
+    });
+}
+
+// Event listener for creating a new project
+const createProjectButton = document.getElementById("createProject");
+createProjectButton.addEventListener("click", () => {
+    // You can implement project creation logic here
+    // Add the new project to the 'projects' array and update the display
+    displayProjects();
+});
+
+// Event listener for assigning a task
+const assignTaskButton = document.getElementById("assignTask");
+assignTaskButton.addEventListener("click", () => {
+    // You can implement task assignment logic here
+    // Add the new task to the 'tasks' array and update the display
+    displayTasks();
+});
+
+// Initial display of projects and tasks
+displayProjects();
+displayTasks();
